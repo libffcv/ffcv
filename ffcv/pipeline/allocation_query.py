7f3b5770eacb281700b4680167168198efda59b2
Andrew Ilyas <ailyas@mit.edu>
2021-10-22 19:55:16 -0400

    make allocation query arguments mandatory

--- ffcv/pipeline/allocation_query.py
+++ ffcv/pipeline/allocation_query.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Union
+from typing import Optional, Tuple, Union
 from dataclasses import dataclass
 
 import numpy as np
@@ -9,6 +9,6 @@ from ffcv.pipeline.stage import Stage
 
 @dataclass(frozen=True)
 class AllocationQuery:
-    shape: Tuple[int, ...] = (1,)
-    dtype: Union[np.dtype, ch.dtype] = np.dtype('<u1')
-    device: ch.device = ch.device('cpu')
+    shape: Tuple[int, ...]
+    dtype: Union[np.dtype, ch.dtype]
+    device: Optional[ch.device] = None
