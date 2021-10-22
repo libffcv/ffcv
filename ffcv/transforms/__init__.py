5df9e6f9365736c429f79ce83da5fa0a2b7ce1f8
Guillaume Leclerc <leclerc@mit.edu>
2021-10-22 00:36:56 -0400

    Put back CIFAR testing

--- ffcv/transforms/__init__.py
+++ ffcv/transforms/__init__.py
@@ -1,6 +1,6 @@
 from .cutout import Cutout
 from .flip import RandomHorizontalFlip
 from .ops import Collate, ToTensor
-from .random_resized_crop import RandomResizedCrop
+# from .random_resized_crop import RandomResizedCrop
 
 __all__ = ['Cutout', 'RandomHorizontalFlip', 'Collate', 'ToTensor']
