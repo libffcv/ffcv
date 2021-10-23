29fc8876125924cf4ddbaf9072a82abfb1e88308
Andrew Ilyas <ailyas@mit.edu>
2021-10-23 16:34:50 -0400

    checking

--- scripts/read.py
+++ scripts/read.py
@@ -9,19 +9,30 @@ from ffcv.transforms import Cutout, Collate, ToTensor, RandomResizedCrop
 import numpy as np
 
 import matplotlib as mpl
+mpl.use('module://imgcat')
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     loader = Loader('/tmp/imagenet_train.beton',
-                    batch_size=512,
+                    batch_size=100,
                     order=OrderOption.RANDOM)
     loader.pipelines['image'] = [
-        # RandomResizedCrop((0.08, 1.0), np.array([2/3., 4/3.]), 224),
-        Collate(),
-        ToTensor()
+        RandomResizedCrop((0.08, 1.0), np.array([2/3., 4/3.]), 224),
+        Collate()
+        # ToTensor()
     ]
     print("Ratio raw", loader.reader.metadata['f0']['mode'].mean())
 
     for i in range(1):
         for image, label in tqdm(loader):
-            pass
+            """
+            for j in range(4):
+                plt.imshow(image[j])
+                plt.show()
+                plt.cla()
+            plt.show()
+            plt.cla()
+            break
+            """
+            import ipdb; ipdb.set_trace()
+            break
