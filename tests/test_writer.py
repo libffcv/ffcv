a75158184d9127b8371bad6b100f0a42bd70bd89
Guillaume Leclerc <leclerc@mit.edu>
2021-10-23 23:20:02 -0400

    More writer/reader tests

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -26,12 +26,22 @@ class DummyDataset(Dataset):
             raise IndexError()
         return (index, np.sin(index))
 
+def validate_simple_dataset(name, length):
+    reader = Reader(name)
+    assert_that(reader.handlers).is_length(2)
+    assert_that(reader.handlers['index']).is_instance_of(IntField)
+    assert_that(reader.handlers['value']).is_instance_of(FloatField)
+    assert_that(reader.alloc_table).is_length(0)
+    assert_that(reader.metadata).is_length(length)
+    assert_that((reader.metadata['f0'] == np.arange(length).astype('int')).all()).is_true()
+    assert_that((np.sin(reader.metadata['f0']) == reader.metadata['f1']).all()).is_true()
+
 def test_write_simple():
     length = 600
     with NamedTemporaryFile() as handle:
         name = handle.name
         dataset = DummyDataset(length)
-        writer = DatasetWriter(600, name, {
+        writer = DatasetWriter(length, name, {
             'index': IntField(),
             'value': FloatField()
         })
@@ -39,12 +49,35 @@ def test_write_simple():
         with writer:
             writer.write_pytorch_dataset(dataset)
 
-        reader = Reader(name)
+        validate_simple_dataset(name, length)
 
-        assert_that(reader.handlers).is_length(2)
-        assert_that(reader.handlers['index']).is_instance_of(IntField)
-        assert_that(reader.handlers['value']).is_instance_of(FloatField)
-        assert_that(reader.alloc_table).is_length(0)
-        assert_that(reader.metadata).is_length(length)
-        assert_that((reader.metadata['f0'] == np.arange(length).astype('int')).all()).is_true()
-        assert_that((np.sin(reader.metadata['f0']) == reader.metadata['f1']).all()).is_true()
+def test_multiple_workers():
+    length = 600
+    with NamedTemporaryFile() as handle:
+        name = handle.name
+        dataset = DummyDataset(length)
+        writer = DatasetWriter(length, name, {
+            'index': IntField(),
+            'value': FloatField()
+        })
+
+        with writer:
+            writer.write_pytorch_dataset(dataset, num_workers=30, chunksize=10000)
+
+        validate_simple_dataset(name, length)
+
+
+def test_super_long():
+    length = 600000
+    with NamedTemporaryFile() as handle:
+        name = handle.name
+        dataset = DummyDataset(length)
+        writer = DatasetWriter(length, name, {
+            'index': IntField(),
+            'value': FloatField()
+        })
+
+        with writer:
+            writer.write_pytorch_dataset(dataset, num_workers=30, chunksize=10000)
+
+        validate_simple_dataset(name, length)
