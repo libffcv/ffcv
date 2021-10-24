49d238b21bdbe5cdf465a373ca70922ccc554be0
Guillaume Leclerc <leclerc@mit.edu>
2021-10-23 20:37:59 -0400

    Half way through full code-gen for the 1st stage

--- ffcv/loader/epoch_iterator.py
+++ ffcv/loader/epoch_iterator.py
@@ -1,7 +1,10 @@
+import ast
 from threading import Thread
 from typing import Sequence, TYPE_CHECKING
 
 import numpy as np
+
+from ffcv.pipeline.compiler import Compiler
 
 from ..utils import chunks
 from ..pipeline.state import Stage
@@ -45,9 +48,56 @@ class EpochIterator(Thread):
             memories_sample.append(pipeline.memory_for_stage(Stage.INDIVIDUAL))
             memories_batch.append(pipeline.memory_for_stage(Stage.BATCH))
             memories_pytorch.append(pipeline.memory_for_stage(Stage.PYTORCH))
-            
-            
+
+
         metadata = self.loader.reader.metadata
+
+        import astor
+        function_calls = []
+        per_sample_namespace = {
+            'my_range': Compiler.get_iterator()
+        }
+        return_values = []
+        for p_ix in range(len(pipelines_sample)):
+            mem_bank_exprs = []
+            pipeline_identifier = f"pipeline_{p_ix}_code"
+            per_sample_namespace[pipeline_identifier] = pipelines_sample[p_ix]
+            for mem_id, mem in enumerate(memories_sample[p_ix]):
+                mem_identifier = f"mem_bank_p_{p_ix}_id_{mem_id}"
+                per_sample_namespace[mem_identifier] = mem
+                if mem is None:
+                    mem_bank_exprs.append(
+                        ast.Constant(value=None)
+                    )
+                else:
+                    mem_bank_exprs.append(
+                        ast.parse(f"{mem_identifier}[batch_slot, dest_ix]")
+                    )
+            return_values.append(ast.Name(id=mem_identifier, ctx=ast.Load()))
+            f_call = ast.Call(func=ast.Name(id=pipeline_identifier, ctx=ast.Load()),
+                              keywords=[],
+                              args=[ast.Name(id='field_value', ctx=ast.Load()), *mem_bank_exprs])
+            f_call = ast.Expr(value=f_call, decorator_list=[], lineno=p_ix+2)
+            function_calls.append(f_call)
+
+        base_code = ast.parse("""
+def compute_samplewise(batch_slot, batch_indices):
+    for ix in my_range(len(batch_indices)):
+        dest_ix = batch_indices[ix]
+        sample = metadata[ix]
+        """)
+        # Append the content of the pipeline to the for loop
+        base_code.body[0].body[0].body.extend(function_calls)
+
+        base_code.body[0].body.append(ast.Return(value=ast.Tuple(elts=return_values,
+                                                                 ctx=ast.Load())))
+        base_code = ast.fix_missing_locations(base_code)
+
+        # Add the proper return statement
+        print(astor.to_source(base_code))
+
+        exec(compile(base_code, '', 'exec'), per_sample_namespace)
+
 
         def compute_sample(batch_slot, batch_indices):
             # For each sample
@@ -65,7 +115,7 @@ class EpochIterator(Thread):
 
                     np.random.seed(int(self.loader.reader.num_samples * self.epoch + ix + self.loader.seed))
                     pipelines_sample[p_ix](field_value, *memory_banks)
-                    
+
             final_result = []
 
             # Because we know that the last per-sample operation is Collate()
