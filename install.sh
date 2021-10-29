1d1814cd2e4f52006b3f0ae536a7c54680224fcd
Logan Engstrom <engstrom@csail.mit.edu>
2021-10-29 01:41:28 -0400

    adde

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 conda create -n ffcv python=3.9
 conda activate ffcv
+
+echo "Activated!"
+
 conda install pytorch==1.9.0 torchvision==0.10 torchaudio cudatoolkit=11.3 compilers pkg-config opencv libjpeg-turbo -c pytorch -c conda-forge
