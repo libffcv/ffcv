8fc0a883033880c0120494383d24f2804896a00f
Guillaume Leclerc <leclerc@mit.edu>
2021-10-30 14:28:43 -0400

    Add test that checks for memory leak
    - Makes sure that when writing the dataset it doesn't get stored in RAM

---
+++ tests/test_memory_leak.py
@@ -0,0 +1,51 @@
+import os
+from tempfile import NamedTemporaryFile
+from time import sleep, time
+import os, psutil
+
+
+import numpy as np
+from tqdm import tqdm
+from assertpy import assert_that
+from torch.utils.data import Dataset
+
+from ffcv.writer import DatasetWriter
+from ffcv.reader import Reader
+from ffcv.fields import BytesField, IntField
+from ffcv.pipeline.compiler import Compiler
+from ffcv.memory_managers.ram import RAMMemoryManager
+from ffcv.libffcv import memcpy
+
+class DummyDataset(Dataset):
+
+    def __init__(self, l, size):
+        self.l = l
+        self.size = size
+
+    def __len__(self):
+        return self.l
+
+    def __getitem__(self, index):
+        if index > self.l:
+            raise IndexError
+        np.random.seed(index)
+        return index, np.random.randint(0, 255, size=self.size, dtype='u1')
+
+def create_and_run(num_samples, size_bytes):
+    name = '/dev/null'
+    dataset = DummyDataset(num_samples, size_bytes)
+    writer = DatasetWriter(num_samples, name, {
+        'index': IntField(),
+        'value': BytesField()
+    })
+
+    with writer:
+        writer.write_pytorch_dataset(dataset, num_workers=-1, chunksize=100)
+    total_dataset_size = num_samples * size_bytes
+    # Dataset should not be in RAM
+    process = psutil.Process(os.getpid())
+    assert_that(process.memory_info().rss).is_less_than(total_dataset_size)
+
+
+def test_memory_leak_write():
+    create_and_run(128100, 500*300*3)
