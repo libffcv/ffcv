ce4e380534593ff7302ebfbfca1bc6ea0b7885e4
Guillaume Leclerc <leclerc@mit.edu>
2021-11-03 02:52:21 -0400

    Allocate each slot independently for pytorch tensors
    - The version number of a tensor is kept on a per-allocation basis. As a
    result pytorch thought the batch we are working on was being written
    over but in fact they were disjoint memory regions

--- ffcv/pipeline/pipeline.py
+++ ffcv/pipeline/pipeline.py
@@ -85,13 +85,16 @@ class Pipeline:
                 final_shape = [batches_ahead,
                                batch_size, *memory_allocation.shape]
                 if isinstance(memory_allocation.dtype, ch.dtype):
-                    result = ch.empty(*final_shape,
-                                      dtype=memory_allocation.dtype,
-                                      device=memory_allocation.device)
-                    try:
-                        result = result.pin_memory()
-                    except:
-                        pass
+                    result = []
+                    for _ in range(final_shape[0]):
+                        partial = ch.empty(*final_shape[1:],
+                                          dtype=memory_allocation.dtype,
+                                          device=memory_allocation.device)
+                        try:
+                            partial = partial.pin_memory()
+                        except:
+                            pass
+                        result.append(partial)
                 else:
                     ch_dtype = ch.from_numpy(np.empty(0, dtype=memory_allocation.dtype)).dtype
                     result = ch.empty(*final_shape,
