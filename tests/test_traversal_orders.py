eeec7999f64c03be017e42cd0f49f5f38f5d2c1c
Guillaume Leclerc <leclerc@mit.edu>
2021-11-04 12:25:29 -0400

    Add quasi random traversal order
    - Performance should be better than pure random
    - Performance will be superior when used in conjunction with slow
      storage memory manager
    - See #29

--- tests/test_traversal_orders.py
+++ tests/test_traversal_orders.py
@@ -3,6 +3,7 @@ from tempfile import TemporaryDirectory
 from os import path
 from typing import Counter
 
+import pytest
 from assertpy import assert_that
 import numpy as np
 from torch.utils.data import Dataset, distributed
@@ -10,9 +11,8 @@ from torch.multiprocessing import spawn, Queue
 from torch.distributed import init_process_group
 
 from ffcv.loader.loader import ORDER_TYPE, OrderOption
-from ffcv.fields.basics import FloatField
 from ffcv.writer import DatasetWriter
-from ffcv.fields import IntField, FloatField
+from ffcv.fields import IntField, BytesField
 from ffcv import Loader
 
 class DummyDataset(Dataset):
@@ -26,12 +26,13 @@ class DummyDataset(Dataset):
     def __getitem__(self, index):
         if index > self.l:
             raise IndexError()
-        return (index, np.sin(index))
+        return (index, np.sin(np.array([index])).view('<u1'))
 
 
 def process_work(rank, world_size, fname, order, sync_fname, out_folder):
     sync_url = f'file://{sync_fname}'
-    init_process_group('nccl', sync_url, rank=rank, world_size=world_size)
+    if world_size > 1:
+        init_process_group('nccl', sync_url, rank=rank, world_size=world_size)
     
     loader = Loader(fname, 8, num_workers=2, order=order,
                     distributed=world_size > 1)
@@ -52,17 +53,17 @@ def prep_and_run_test(num_workers, order):
             dataset = DummyDataset(length)
             writer = DatasetWriter(length, name, {
                 'index': IntField(),
-                'value': FloatField()
+                'value': BytesField()
             })
 
             with writer:
                 writer.write_pytorch_dataset(dataset)
                 
-            spawn(process_work, nprocs=num_workers, args=(num_workers,
-                                                          name,
-                                                          order,
-                                                          sync_file,
-                                                          folder))
+            args = (num_workers, name, order, sync_file, folder)
+            if num_workers > 1:
+                spawn(process_work, nprocs=num_workers, args=args)
+            else:
+                process_work(*((0, ) + args))
             
             results = []
             for r in range(num_workers):
@@ -74,7 +75,7 @@ def prep_and_run_test(num_workers, order):
             for i in range(results.shape[0]):
                 if order == OrderOption.SEQUENTIAL and i < results.shape[0] - 1:
                     assert_that((results[i] == results[i + 1]).all()).is_true()
-                if order == OrderOption.RANDOM and i < results.shape[0] - 1:
+                if order != OrderOption.SEQUENTIAL and i < results.shape[0] - 1:
                     assert_that((results[i] == results[i + 1]).all()).is_false()
                     
                 epoch_content = Counter(results[i])
@@ -85,26 +86,41 @@ def prep_and_run_test(num_workers, order):
         
         
 
-def test_distributed_sequential_1():
+def test_traversal_sequential_1():
     prep_and_run_test(1, OrderOption.SEQUENTIAL)
 
-def test_distributed_sequential_2():
+def test_traversal_sequential_2():
     prep_and_run_test(2, OrderOption.SEQUENTIAL)
 
-def test_distributed_sequential_3():
+def test_traversal_sequential_3():
     prep_and_run_test(3, OrderOption.SEQUENTIAL)
 
-def test_distributed_sequential_4():
+def test_traversal_sequential_4():
     prep_and_run_test(4, OrderOption.SEQUENTIAL)
 
-def test_distributed_random_1():
+def test_traversal_random_1():
     prep_and_run_test(1, OrderOption.RANDOM)
 
-def test_distributed_random_2():
+def test_traversal_random_2():
     prep_and_run_test(2, OrderOption.RANDOM)
 
-def test_distributed_random_3():
+def test_traversal_random_3():
     prep_and_run_test(3, OrderOption.RANDOM)
 
-def test_distributed_random_4():
+def test_traversal_random_4():
     prep_and_run_test(4, OrderOption.RANDOM)
+
+def test_traversal_quasirandom_1():
+    prep_and_run_test(1, OrderOption.QUASI_RANDOM)
+
+@pytest.mark.skip()
+def test_traversal_quasirandom_2():
+    prep_and_run_test(2, OrderOption.QUASI_RANDOM)
+
+@pytest.mark.skip()
+def test_traversal_quasirandom_3():
+    prep_and_run_test(3, OrderOption.QUASI_RANDOM)
+
+@pytest.mark.skip()
+def test_traversal_quasirandom_4():
+    prep_and_run_test(4, OrderOption.QUASI_RANDOM)
