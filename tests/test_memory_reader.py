a9eaa9c76087804f150dcff25d0c8ea2107816f7
Guillaume Leclerc <leclerc@mit.edu>
2021-11-09 14:44:58 -0500

    Temp commit to switch to another machine

--- tests/test_memory_reader.py
+++ tests/test_memory_reader.py
@@ -13,7 +13,7 @@ from ffcv.writer import DatasetWriter
 from ffcv.reader import Reader
 from ffcv.fields import BytesField, IntField
 from ffcv.pipeline.compiler import Compiler
-from ffcv.memory_managers.ram import RAMMemoryManager
+from ffcv.memory_managers import OSCacheManager
 
 from test_memory_allocation import DummyDataset
 
@@ -33,26 +33,27 @@ def create_and_validate(length, size, do_compile):
             writer.write_pytorch_dataset(dataset, num_workers=2, chunksize=5)
 
         reader = Reader(name)
-        manager = RAMMemoryManager(reader)
+        manager = OSCacheManager(reader)
+        context = manager.schedule_epoch(np.array([0, 1]))
 
         indices = np.random.choice(length, 500)
         addresses = reader.alloc_table['ptr'][indices]
         sample_ids = reader.alloc_table['sample_id'][indices]
 
         Compiler.set_enabled(do_compile)
+        read_fn = manager.compile_reader()
 
-        with manager:
-            read_fn = manager.compile_reader()
+        with context:
 
-        for addr, sample_id in zip(tqdm(addresses), sample_ids):
-            read_buffer = read_fn(addr, manager.state)
-            np.random.seed(sample_id)
-            expected_buff = np.random.randint(0, 255, size=size, dtype='u1')
+            for addr, sample_id in zip(tqdm(addresses), sample_ids):
+                read_buffer = read_fn(addr, context.state)
+                np.random.seed(sample_id)
+                expected_buff = np.random.randint(0, 255, size=size, dtype='u1')
 
-            assert_that(read_buffer).is_length(len(expected_buff))
-            assert_that(np.all(read_buffer == expected_buff)).is_true()
+                assert_that(read_buffer).is_length(len(expected_buff))
+                assert_that(np.all(read_buffer == expected_buff)).is_true()
 
-        # We skip the first which is compilation
+            # We skip the first which is compilation
 
 def test_simple():
     create_and_validate(600, 76, False)
