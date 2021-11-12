06eee1d9d0fb24136f3bca5d8709605c2fff5104
alaakh <alaakh@mit.edu>
2021-11-12 11:27:21 -0500

    Test cases for standard train and val transforms

--- tests/test_in_transforms_pipeline.py
+++ tests/test_in_transforms_pipeline.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tempfile import NamedTemporaryFile
+from numpy.lib.npyio import load
 
 import torch as ch
 from torch.utils.data import Dataset
@@ -42,99 +43,82 @@ class DummyDataset(Dataset):
             result = tuple(reversed(result))
         return result
 
-
-def test_train_loader(length=10, batch_size=2, num_workers=8, reversed=False):
-
-    dataset = DummyDataset(length, 500, 300, reversed=reversed)
-
-    with NamedTemporaryFile() as handle:
-        name = handle.name
-
-        fields = {
-            'label': IntField(),
-            'image': RGBImageField(write_mode="raw", max_resolution=256, smart_threshold=3)
-        }
-
-        if reversed:
-
-            fields = {
-                'image': RGBImageField(write_mode="raw", max_resolution=256, smart_threshold=3),
-                'label': IntField()
-            }
-
-
-        writer = DatasetWriter(length, name, fields)
-
-        with writer:
-            writer.write_pytorch_dataset(dataset, num_workers=2, chunksize=5)
-
-        Compiler.set_enabled(False)
-
-        loader = Loader(name,
-                        batch_size=batch_size,
-                        num_workers=num_workers,
-                        order=OrderOption.QUASI_RANDOM,
-                        pipelines={
-                            "image": [
-                                RandomResizedCropRGBImageDecoder((224, 224)),
-                                RandomHorizontalFlip(0.5),
-                                ToTensor(),
-                                ToDevice(ch.device('cuda:0')),
-                                ToTorchImage(),
-                                Convert(ch.float16),
-                                Normalize((IMAGENET_MEAN * 255).tolist(), (IMAGENET_STD * 255).tolist()),
-                            ],
-                            "label": [IntDecoder(), ToTensor(), Squeeze(), ToDevice(ch.device("cuda:0"))]
-                        }
-                )
-
-        for X, y in loader:
-            pass
-
-
-def test_val_loader(length=10, batch_size=2, num_workers=8):
-
-    dataset = DummyDataset(length, 500, 300, reversed=reversed)
-
-    with NamedTemporaryFile() as handle:
-        name = handle.name
-
-        fields = {
-            'label': IntField(),
-            'image': RGBImageField(write_mode="raw", max_resolution=256, smart_threshold=3)
-        }
-
-        if reversed:
-
-            fields = {
-                'image': RGBImageField(write_mode="raw", max_resolution=256, smart_threshold=3),
-                'label': IntField()
-            }
-
-
-        writer = DatasetWriter(length, name, fields)
-
-        with writer:
-            writer.write_pytorch_dataset(dataset, num_workers=2, chunksize=5)
-
-        Compiler.set_enabled(False)
-
-        loader = Loader(name,
+def create_train_loader(name, batch_size, num_workers):
+    loader = Loader(name,
                     batch_size=batch_size,
                     num_workers=num_workers,
                     order=OrderOption.QUASI_RANDOM,
                     pipelines={
                         "image": [
-                            CenterCropRGBImageDecoder(output_size=(224,224), ratio=DEFAULT_CROP_RATIO),
+                            RandomResizedCropRGBImageDecoder((224, 224)),
+                            RandomHorizontalFlip(0.5),
                             ToTensor(),
                             ToDevice(ch.device('cuda:0')),
                             ToTorchImage(),
                             Convert(ch.float16),
                             Normalize((IMAGENET_MEAN * 255).tolist(), (IMAGENET_STD * 255).tolist()),
                         ],
-                        "label": [IntDecoder(), ToTensor(), Squeeze(), ToDevice(ch.device("cuda:0"), non_blocking=True)]
+                        "label": [IntDecoder(), ToTensor(), Squeeze(), ToDevice(ch.device("cuda:0"))]
                     }
             )
+    return loader
+
+def create_val_loader(name, batch_size, num_workers):
+    loader = Loader(name,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            order=OrderOption.QUASI_RANDOM,
+            pipelines={
+                "image": [
+                    CenterCropRGBImageDecoder(output_size=(224,224), ratio=DEFAULT_CROP_RATIO),
+                    ToTensor(),
+                    ToDevice(ch.device('cuda:0')),
+                    ToTorchImage(),
+                    Convert(ch.float16),
+                    Normalize((IMAGENET_MEAN * 255).tolist(), (IMAGENET_STD * 255).tolist()),
+                ],
+                "label": [IntDecoder(), ToTensor(), Squeeze(), ToDevice(ch.device("cuda:0"), non_blocking=True)]
+            }
+    )
+    return loader
+
+def loop_loader(length=10, batch_size=2, num_workers=8, reversed=False, train=True):
+
+    dataset = DummyDataset(length, 500, 300, reversed=reversed)
+
+    with NamedTemporaryFile() as handle:
+        name = handle.name
+
+        fields = {
+            'label': IntField(),
+            'image': RGBImageField(write_mode="raw", max_resolution=256, smart_threshold=3)
+        }
+
+        if reversed:
+
+            fields = {
+                'image': RGBImageField(write_mode="raw", max_resolution=256, smart_threshold=3),
+                'label': IntField()
+            }
+
+
+        writer = DatasetWriter(length, name, fields)
+
+        with writer:
+            writer.write_pytorch_dataset(dataset, num_workers=2, chunksize=5)
+
+        Compiler.set_enabled(True)
+
+        if train:
+            loader = create_train_loader(name, batch_size, num_workers)
+        else:
+            loader = create_val_loader(name, batch_size, num_workers)
 
         for X, y in loader:
             pass
+
+def test_train_loader():
+    loop_loader(length=100, batch_size=16, train=True)
+
+def test_val_loader():
+    loop_loader(length=100, batch_size=16, train=False)
