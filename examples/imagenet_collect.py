42c1f93136351919b3727e42cbf1857966d5f750
Logan Engstrom <engstrom@csail.mit.edu>
2021-11-12 02:07:17 -0500

    added distributed training

--- examples/imagenet_collect.py
+++ examples/imagenet_collect.py
@@ -1,4 +1,5 @@
 import subprocess
+from collections import OrderedDict, defaultdict
 from uuid import uuid4
 import matplotlib as mpl
 mpl.use('module://imgcat')
@@ -43,19 +44,49 @@ def main(log_dir):
         all_logs.append(logs)
 
     logs = pd.concat(all_logs)
-    fig, ax = plt.subplots()
+    logs['delta'] = logs['training.epochs'] - logs['resolution.end_ramp']
+    def plot_all(x='epoch', by_col=None):
+        fig, ax = plt.subplots()
+        print(by_col)
+        labeled = {}
+        if by_col is not None:
+            colors = ['red', 'blue', 'orange', 'purple', 'yellow', 'green', 'cyan']
+            mapping = {}
+            for i, uu in enumerate(logs[by_col].unique()):
+                mapping[uu] = colors[i]
+            labeled = defaultdict(lambda:False)
 
-    for name, group in logs.groupby('uid'):
-        group.plot.line(x='epoch', y='top_1', ax=ax)
-    plt.show()
+        for name, group in logs.groupby('uid'):
+            kwargs = {}
+            if by_col is not None:
+                value = group.iloc[0][by_col]
+                row_color = mapping[value]
 
-    fig, ax = plt.subplots()
-    for name, group in logs.groupby('uid'):
-        group.plot.line(x='relative_time', y='top_1', ax=ax)
-    plt.show()
+                kwargs['color'] = row_color
+                if not labeled[value]:
+                    labeled[value] = True
+                    kwargs['label'] = value
+                else:
+                    kwargs['label'] = False
+                    kwargs['legend'] = False
+
+            group.plot.line(x=x, y='top_1', ax=ax, **kwargs)
+
+        plt.show()
+
+    plot_all(x='epoch')
+    plot_all(x='relative_time')
+
+    for chungus in ['delta', 'training.lr', 'training.weight_decay',
+                    'resolution.max_res', 'resolution.min_res']:
+        plot_all(x='relative_time', by_col=chungus)
+
+    plot_all()
+
     last = logs.sort_values('epoch').groupby('uid').last()
     keep = ['relative_time', 'epoch', 'top_1', 'top_5', 'training.batch_size',
-    'resolution.max_res', 'resolution.min_res', 'model.antialias', 'resolution.end_ramp', 'training.weight_decay']
+    'resolution.max_res', 'resolution.min_res', 'model.antialias',
+    'resolution.end_ramp', 'training.weight_decay']
     last = last[keep].sort_values('top_5')
     print(logs)
     print(last)
