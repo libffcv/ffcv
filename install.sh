363d37308e3ccc6befaf57d9e3795f850c2a1646
Guillaume Leclerc <leclerc@mit.edu>
2021-11-13 12:34:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,1 @@
-conda create -n ffcv python=3.9
-conda activate ffcv
-
-echo "Activated!"
-
-conda install compilers pkg-config libjpeg-turbo -c conda-forge
-conda install pytorch torchvision cudatoolkit=11.3 -c pytorch-nightly
-# conda install pytorch torchvision==0.10 torchaudio cudatoolkit=11.3 compilers pkg-config libjpeg-turbo opencv -c pytorch-nightly -c conda-forge
+conda create -n ffcv python=3.9 pkg-config libjpeg-turbo opencv pytorch torchvision cudatoolkit=11.3 numba  -c pytorch-nightly -c conda-forge
