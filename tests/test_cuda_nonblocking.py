40a1eb943f2752fd722b4ad48bb8734d6cf709ca
Andrew Ilyas <ailyas@mit.edu>
2021-11-25 17:56:37 -0500

    weird

--- tests/test_cuda_nonblocking.py
+++ tests/test_cuda_nonblocking.py
@@ -1,6 +1,7 @@
 from tempfile import NamedTemporaryFile
 import torch as ch
 from tqdm import tqdm
+import time
 
 from assertpy import assert_that
 import numpy as np
@@ -28,54 +29,46 @@ class DummyArrayDataset(Dataset):
         np.random.seed(index)
         return (np.random.rand(50000) > 0.5).astype('bool'), np.random.rand(50000).astype('float32'), index
 
-def test_cuda():
-    n_samples, shape = (300000, (50000,))
-    with NamedTemporaryFile() as handle:
-        # name = handle.name
-        # dataset = DummyArrayDataset(n_samples, shape)
-        # writer = DatasetWriter(len(dataset), name, {
-        #     'mask': NDArrayField(dtype=np.dtype('bool'), shape=(50_000,)),
-        #     'targets': NDArrayField(dtype=np.dtype('float32'), shape=(50_000,)),
-        #     'idx': IntField()
-        # })
+def run_experiment_cuda(weight, loader, sync=False):
+    total = 0.
+    for X_bool, _, __ in tqdm(loader):
+        if sync:
+            ch.cuda.synchronize()
+            time.sleep(1)
+        total += X_bool.float() @ weight
 
-        # with writer:
-        #     writer.write_pytorch_dataset(dataset, num_workers=10)
+    return total
+
+def run_cuda(weight, sync):
+    n_samples, shape = (2048 * 10, (50000,))
+    with NamedTemporaryFile() as handle:
+        name = handle.name
+        dataset = DummyArrayDataset(n_samples, shape)
+        writer = DatasetWriter(len(dataset), name, {
+            'mask': NDArrayField(dtype=np.dtype('bool'), shape=(50_000,)),
+            'targets': NDArrayField(dtype=np.dtype('float32'), shape=(50_000,)),
+            'idx': IntField()
+        })
+
+        with writer:
+            writer.write_pytorch_dataset(dataset, num_workers=10)
 
         loader = Loader(
-            '/mnt/nfs/home/aiilyas/subpops_betons/train_confs_50.beton',
-                # name,
+                name,
                 batch_size=2048,
                 num_workers=10,
                 order=OrderOption.QUASI_RANDOM,
                 indices=np.arange(n_samples),
-                drop_last=True,
+                drop_last=False,
                 os_cache=True,
                 pipelines={
-                    'mask': [NDArrayDecoder(), ToTensor(), ToDevice(ch.device('cuda:0'), non_blocking=True)],
-                    'targets': [NDArrayDecoder(), ToTensor(), ToDevice(ch.device('cuda:0'), non_blocking=True)],
-                    'idx': [IntDecoder(), ToTensor(), Squeeze(), ToDevice(ch.device('cuda:0'), non_blocking=True)]
+                    'mask': [NDArrayDecoder(), ToTensor(), ToDevice(ch.device('cuda:0'), non_blocking=False)],
+                    'targets': [NDArrayDecoder(), ToTensor(), ToDevice(ch.device('cuda:0'), non_blocking=False)],
+                    'idx': [IntDecoder(), ToTensor(), Squeeze(), ToDevice(ch.device('cuda:0'), non_blocking=False)]
                 })
-
-        indexer = ch.rand(300000, 50000)
-        res = ch.empty(2048, 50000).cuda()
-
-        chungus = ch.randn(50000, 50000).cuda()
-        w_saga = ch.randn(50000, 50000).cuda()
-        for X_bool, targs, ixes in tqdm(loader):
-            a_i = indexer[ixes].cuda(non_blocking=True)
-            X = X_bool.float()
-            X_bool.logical_not_()
-            
-            X -= 0.5
-            X /= 0.5
-
-            B = X_bool.sum(0)
-            ch.mm(X, chungus, out=res)
-            res -= targs
-
-            res *= X_bool
-            res -= a_i
-            ch.mm(X.T, res, out=w_saga)
-
         
+        return run_experiment_cuda(weight, loader, sync)
+
+weight = ch.randn(50_000, 50_000).cuda()
+for t in [False, True, True]:
+    print(run_cuda(weight, t))
