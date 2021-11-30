238eda48d1b0c157c28f1bad18502f0565fce44a
Guillaume Leclerc <leclerc@mit.edu>
2021-11-30 17:38:45 -0500

    First pediting test for webdataset

--- ffcv/writer.py
+++ ffcv/writer.py
@@ -1,22 +1,46 @@
-from typing import List, Mapping
+from functools import partial
+from typing import Callable, List, Mapping
 from os import SEEK_END, path
 import numpy as np
 from time import sleep
+from concurrent.futures import ThreadPoolExecutor
 import ctypes
 from multiprocessing import (shared_memory, cpu_count, Queue, Process, Value)
 
 from tqdm import tqdm
+from tqdm.contrib.concurrent import thread_map
 
 from .utils import chunks, is_power_of_2
 from .fields.base import Field
 from .memory_allocator import MemoryAllocator
 from .types import (TYPE_ID_HANDLER, get_metadata_type, HeaderType,
                     FieldDescType, CURRENT_VERSION, ALLOC_TABLE_TYPE)
-                    
+
 
 MIN_PAGE_SIZE = 1 << 21  # 2MiB, which is the most common HugePage size
 
-def handle_sample(sample, dest_ix, field_name, metadata, allocator):
+def from_shard(shard, pipeline):
+    # We import webdataset here so that it desn't crash if it's not required
+    # (Webdataset is an optional depdency)
+    from webdataset import WebDataset
+
+    dataset = WebDataset(shard)
+    dataset = pipeline(dataset)
+    return dataset
+
+def count_samples_in_shard(shard, pipeline):
+    #
+    # We count the length of the dataset
+    # We are not using __len__ since it might not be implemented
+    count = 0
+    for _ in from_shard(shard, pipeline):
+        count += 1
+
+    return count
+
+
+def handle_sample(sample, dest_ix, field_names, metadata, allocator, fields):
+    # We should only have to retry at least one
     for i in range(2):
         try:
             allocator.set_current_sample(dest_ix)
@@ -34,9 +58,41 @@ def handle_sample(sample, dest_ix, field_name, metadata, allocator):
             if i == 1:
                 raise
 
+def worker_job_webdataset(input_queue, metadata_sm, metadata_type, fields,
+               allocator, done_number, allocations_queue, pipeline):
+
+    metadata = np.frombuffer(metadata_sm.buf, dtype=metadata_type)
+    field_names = metadata_type.names
+
+    # This `with` block ensures that all the pages allocated have been written
+    # onto the file
+    with allocator:
+        while True:
+            todo = input_queue.get()
+
+            if todo is None:
+                # No more work left to do
+                break
+
+            shard, offset = todo
+
+            # For each sample in the chunk
+            done = 0
+            for i, sample in enumerate(from_shard(shard, pipeline)):
+                done += 1
+                dest_ix = offset + i
+                handle_sample(sample, dest_ix, field_names, metadata, allocator, fields)
+
+            # We warn the main thread of our progress
+            with done_number.get_lock():
+                done_number.value += done
+
+    allocations_queue.put(allocator.allocations)
+
+
 
 def worker_job_indexed_dataset(input_queue, metadata_sm, metadata_type, fields,
-               allocator, done_number, dataset, allocations_queue):
+               allocator, done_number, allocations_queue, dataset):
 
     metadata = np.frombuffer(metadata_sm.buf, dtype=metadata_type)
     field_names = metadata_type.names
@@ -53,9 +109,8 @@ def worker_job_indexed_dataset(input_queue, metadata_sm, metadata_type, fields,
 
             # For each sample in the chunk
             for dest_ix, source_ix in chunk:
-                # We should only have to retry at least one
                 sample = dataset[source_ix]
-                handle_sample(sample, dest_ix, field_name, metadata, allocator)
+                handle_sample(sample, dest_ix, field_names, metadata, allocator, fields)
 
             # We warn the main thread of our progress
             with done_number.get_lock():
@@ -65,12 +120,16 @@ def worker_job_indexed_dataset(input_queue, metadata_sm, metadata_type, fields,
 
 
 class DatasetWriter():
-    def __init__(self, num_samples: int, fname: str, fields: Mapping[str, Field],
-                 page_size: int = 4 * MIN_PAGE_SIZE):
-        self.num_samples = num_samples
+    def __init__(self, fname: str, fields: Mapping[str, Field],
+                 page_size: int = 4 * MIN_PAGE_SIZE, num_workers: int = -1):
         self.fields = fields
         self.fname = fname
         self.metadata_type = get_metadata_type(list(self.fields.values()))
+
+        self.num_workers = num_workers
+        # We use all cores by default
+        if self.num_workers < 1:
+            self.num_workers = cpu_count()
 
         if not is_power_of_2(page_size):
             raise ValueError(f'page_size isnt a power of 2')
@@ -79,11 +138,9 @@ class DatasetWriter():
 
         self.page_size = page_size
 
+    def prepare(self):
 
-    def __enter__(self):
         with open(self.fname, 'wb') as fp:
-
-
             # Prepare the header data
             header = np.zeros(1, dtype=HeaderType)[0]
             header['version'] = CURRENT_VERSION
@@ -102,7 +159,7 @@ class DatasetWriter():
             fields_descriptor = np.zeros(len(self.fields),
                                               dtype=FieldDescType)
             field_type_to_type_id = {v: k for (k, v) in TYPE_ID_HANDLER.items()}
-            
+
             fieldname_max_len = fields_descriptor[0]['name'].shape[0]
 
             for i, (name, field) in enumerate(self.fields.items()):
@@ -117,9 +174,6 @@ class DatasetWriter():
 
             fp.write(fields_descriptor.tobytes())
 
-
-        # Makes a memmap to the metadata for the samples
-
         total_metadata_size = self.num_samples * self.metadata_type.itemsize
 
         # Shared memory for all the writers to fill the information
@@ -130,34 +184,20 @@ class DatasetWriter():
                                                       size=total_metadata_size)
 
         self.data_region_start = self.metadata_start + total_metadata_size
-        self.allocation_list = []
 
 
+    def _write_common(self, num_samples, queue_content, work_fn, extra_worker_args):
+        self.num_samples = num_samples
 
-    def write_pytorch_dataset(self, dataset, num_workers=-1,
-                              indices: List[int]=None, chunksize=100,
-                              shuffle_indices: bool = False):
+        self.prepare()
+        allocation_list = []
 
-        # We use all cores by default
-        if num_workers == -1:
-            num_workers = cpu_count()
-
-        # If the user didn't specify an order we just add samples
-        # sequentially
-        if indices is None:
-            indices = np.arange(self.num_samples)
-            
-        if shuffle_indices:
-            np.random.shuffle(indices)
-            
-        # We add indices to the indices so that workers know where
-        # to write in the metadata array
-        indices: List[int] = list(enumerate(indices))
+        # Makes a memmap to the metadata for the samples
 
         # We publish all the work that has to be done into a queue
         workqueue: Queue = Queue()
-        for chunk in chunks(indices, chunksize):
-            workqueue.put(chunk)
+        for todo in queue_content:
+            workqueue.put(todo)
 
         # This will contain all the memory allocations each worker
         # produced. This will go at the end of the file
@@ -165,7 +205,7 @@ class DatasetWriter():
 
         # We add a token for each worker to warn them that there
         # is no more work to be done
-        for _ in range(num_workers):
+        for _ in range(self.num_workers):
             workqueue.put(None)
 
         # Define counters we need to orchestrate the workers
@@ -178,11 +218,11 @@ class DatasetWriter():
         worker_args = (workqueue, self.metadata_sm,
                        self.metadata_type, self.fields,
                        allocator, done_number,
-                       dataset, allocations_queue)
+                       allocations_queue, *extra_worker_args)
 
         # Create the workers
-        processes = [Process(target=worker_job_indexed_dataset, args=worker_args)
-                     for _ in range(num_workers)]
+        processes = [Process(target=work_fn, args=worker_args)
+                     for _ in range(self.num_workers)]
         # start the workers
         for p in processes: p.start()
         # Wait for all the workers to be done
@@ -200,10 +240,46 @@ class DatasetWriter():
         progress.close()
 
         # Wait for all the workers to be done and get their allocations
-        self.allocation_list.extend([allocations_queue.get() for p in processes])
+        allocation_list.extend([allocations_queue.get() for p in processes])
 
-    def finalize(self) :
-        allocations = self.allocation_list
+        self.finalize(allocation_list)
+        self.metadata_sm.close()
+        self.metadata_sm.unlink()
+
+
+    def from_indexed_dataset(self, dataset,
+                              indices: List[int]=None, chunksize=100,
+                              shuffle_indices: bool = False):
+
+        # If the user didn't specify an order we just add samples
+        # sequentially
+        if indices is None:
+            indices = np.arange(self.num_samples)
+
+        if shuffle_indices:
+            np.random.shuffle(indices)
+
+        # We add indices to the indices so that workers know where
+        # to write in the metadata array
+        indices: List[int] = list(enumerate(indices))
+
+        self._write_common(len(indices), chunks(indices, chunksize),
+                           worker_job_indexed_dataset, (dataset, ))
+
+
+    def from_webdataset(self, shards: List[str], pipeline: Callable):
+        # executor = ThreadPoolExecutor(self.num_workers)
+        counter = partial(count_samples_in_shard, pipeline=pipeline)
+        lengths = thread_map(counter, shards, max_workers=self.num_workers)
+        total_len = sum(lengths)
+
+        offsets = np.cumsum([0] + lengths)[:-1]
+
+        todos = zip(shards, offsets)
+        self._write_common(total_len, todos, worker_job_webdataset, (pipeline, ))
+
+
+    def finalize(self, allocations) :
         # Writing metadata
         with open(self.fname, 'r+b') as fp:
             fp.seek(self.metadata_start)
@@ -227,10 +303,3 @@ class DatasetWriter():
             fp.seek(0)
             # And write the header
             fp.write(self.header.tobytes())
-
-
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.finalize()
-        self.metadata_sm.close()
-        self.metadata_sm.unlink()
