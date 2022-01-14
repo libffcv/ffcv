d00a7f7b972b6747aaf454e960b9d705a88112ab
sung-max <samparkmit@gmail.com>
2022-01-13 20:31:40 -0500

    cifar train code update

--- examples/train_cifar.py
+++ examples/train_cifar.py
@@ -1,13 +1,20 @@
+from argparse import ArgumentParser
 from typing import List
+from tempfile import NamedTemporaryFile
 import time
-import torch as ch
-import torchvision 
-
-# Make optimizer and schedule
-from torch.cuda.amp import GradScaler, autocast
-from torch.optim import SGD, lr_scheduler
-from torch.nn import CrossEntropyLoss, Conv2d, BatchNorm2d
 import numpy as np
+from tqdm import tqdm
+
+import torch as ch
+from torch.cuda.amp import GradScaler, autocast
+from torch.nn import CrossEntropyLoss, Conv2d, BatchNorm2d
+from torch.optim import SGD, lr_scheduler
+import torchvision
+
+from fastargs import get_current_config
+from fastargs.decorators import param
+from fastargs import Param, Section
+from fastargs.validation import And, OneOf
 
 from ffcv.pipeline.operation import Operation
 from ffcv.transforms.common import Squeeze
@@ -18,21 +25,20 @@ from ffcv.transforms import RandomHorizontalFlip, Cutout, \
 from ffcv.fields.decoders import IntDecoder, SimpleRGBImageDecoder
 from ffcv.fields import IntField, RGBImageField
 
-from fastargs import get_current_config
-from fastargs.decorators import param
-from fastargs import Param, Section
-from fastargs.validation import And, OneOf
-from argparse import ArgumentParser
-
-from tempfile import NamedTemporaryFile
-
 Section('params', 'hyperparameters').params(
     lr=Param(float, 'the learning rate to use', required=True),
     num_epochs=Param(int, 'number of epochs to run for', required=True),
+    batch_size=Param(int, 'batch size', default=512),
+    momentum=Param(float, 'momentum for SGD', default=0.9),
+    weight_decay=Param(float, 'l2 weight decay', default=5e-4),
     label_smoothing=Param(float, 'value of label smoothing', default=0.1),
+    num_workers=Param(int, 'The number of workers', default=8),
+    lr_tta=Param(int, 'should do lr flipping/avging at test time', default=1)
 )
 
-def make_datasets(data_path):
+@param('params.batch_size')
+@param('params.num_workers')
+def make_datasets(data_path, batch_size=None, num_workers=None):
     datasets = {'train': torchvision.datasets.CIFAR10('/tmp', train=True, download=True),
         'test': torchvision.datasets.CIFAR10('/tmp', train=False, download=True)}
 
@@ -65,7 +71,7 @@ def make_datasets(data_path):
             torchvision.transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
         ])
 
-        loaders[name] = Loader(data_path + name, batch_size=512, num_workers=8,
+        loaders[name] = Loader(data_path + name, batch_size=batch_size, num_workers=num_workers,
                             order=OrderOption.RANDOM, drop_last=(name == 'train'),
                             pipelines={'image': image_pipeline, 'label': label_pipeline})
 
@@ -89,7 +95,7 @@ class Residual(ch.nn.Module):
 
 def conv_bn(channels_in, channels_out, kernel_size=3, stride=1, padding=1, groups=1):
     return ch.nn.Sequential(
-            ch.nn.Conv2d(channels_in, channels_out, kernel_size=kernel_size, 
+            ch.nn.Conv2d(channels_in, channels_out, kernel_size=kernel_size,
                          stride=stride, padding=padding, groups=groups, bias=False),
             ch.nn.BatchNorm2d(channels_out),
             ch.nn.ReLU(inplace=True)
@@ -115,19 +121,22 @@ def construct_model():
 
 @param('params.lr')
 @param('params.num_epochs')
+@param('params.momentum')
+@param('params.weight_decay')
 @param('params.label_smoothing')
-def train(model, loaders, lr=None, num_epochs=None, label_smoothing=None):
-    opt = SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
+def train(model, loaders, lr=None, num_epochs=None, label_smoothing=None,
+          momentum=None, weight_decay=None):
+    opt = SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
     total_iters = len(loaders['train']) * num_epochs
     scheduler = lr_scheduler.LambdaLR(opt, lambda step: 1 - float(step) / total_iters)
     scaler = GradScaler()
     loss_fn = CrossEntropyLoss(label_smoothing=label_smoothing)
 
     for _ in range(num_epochs):
-        for ims, labs in loaders['train']:
+        for ims, labs in tqdm(loaders['train']):
             opt.zero_grad(set_to_none=True)
             with autocast():
-                out = model(ims) 
+                out = model(ims)
                 loss = loss_fn(out, labs)
 
             scaler.scale(loss).backward()
@@ -135,21 +144,24 @@ def train(model, loaders, lr=None, num_epochs=None, label_smoothing=None):
             scaler.update()
             scheduler.step()
 
-def evaluate(model, loaders):
+@param('params.lr_tta')
+def evaluate(model, loaders, lr_tta=False):
     model.eval()
     with ch.no_grad():
         for name in ['train', 'test']:
             total_correct, total_num = 0., 0.
-            for ims, labs in loaders[name]:
+            for ims, labs in tqdm(loaders[name]):
                 with autocast():
-                    out = (model(ims) + model(ch.fliplr(ims))) / 2.
+                    out = model(ims)
+                    if lr_tta:
+                        out += model(ch.fliplr(ims))
                     total_correct += out.argmax(1).eq(labs).sum().cpu().item()
                     total_num += ims.shape[0]
             print(f'{name} accuracy: {total_correct / total_num * 100:.1f}%')
 
 if __name__ == "__main__":
     config = get_current_config()
-    parser = ArgumentParser(description='Fast imagenet training')
+    parser = ArgumentParser(description='Fast CIFAR-10 training')
     config.augment_argparse(parser)
     config.collect_argparse_args(parser)
     config.validate(mode='stderr')
@@ -159,5 +171,5 @@ if __name__ == "__main__":
         loaders, start_time = make_datasets(handle.name)
         model = construct_model()
         train(model, loaders)
-        print('Total time: ', time.time() - start_time)
+        print(f'Total time: {time.time() - start_time:.5f}')
         evaluate(model, loaders)
