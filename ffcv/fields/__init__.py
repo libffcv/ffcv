0dfcbaa0317e4d146ac77a4ffa261721c8771863
Guillaume Leclerc <leclerc@mit.edu>
2022-01-28 21:47:14 -0500

    Add TorchTensorField
    - Resolves #101

--- ffcv/fields/__init__.py
+++ ffcv/fields/__init__.py
@@ -2,8 +2,8 @@ from .base import Field
 from .basics import FloatField, IntField
 from .rgb_image import RGBImageField
 from .bytes import BytesField
-from .ndarray import NDArrayField
+from .ndarray import NDArrayField, TorchTensorField
 from .json import JSONField
 
 __all__ = ['Field', 'BytesField', 'IntField', 'FloatField', 'RGBImageField',
-           'NDArrayField', 'JSONField']
+           'NDArrayField', 'JSONField', 'TorchTensorField']
