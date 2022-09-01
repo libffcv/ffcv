e5e95fd7e21c71b754aa772957af9e84cc26bdcb
Philip Meier <github.pmeier@posteo.de>
2022-09-01 14:42:58 +0200

    increment loading index regardless of CUDA

--- ffcv/loader/epoch_iterator.py
+++ ffcv/loader/epoch_iterator.py
@@ -98,7 +98,7 @@ class EpochIterator(Thread):
                     event = ch.cuda.Event()
                     event.record(ch.cuda.default_stream())
                     events[just_finished_slot] = event
-                    b_ix += 1
+                b_ix += 1
 
         except StopIteration:
             self.output_queue.put(None)
