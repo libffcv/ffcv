695c563bd1ca8be013aac35a65d1b57958b7ac74
Benjamin Warner <me@benjaminwarner.dev>
2023-03-02 15:58:59 -0600

    Modify tests to work on consumer hardware

--- tests/test_json_field.py
+++ tests/test_json_field.py
@@ -2,7 +2,7 @@ import string
 from ctypes import pointer
 from tempfile import NamedTemporaryFile
 from collections import defaultdict
-from assertpy.assertpy import assert_that
+from multiprocessing import cpu_count
 
 from assertpy import assert_that
 import numpy as np
@@ -46,11 +46,11 @@ def run_test(n_samples):
         writer = DatasetWriter(name, {
             'index': IntField(),
             'activations': JSONField()
-        }, num_workers=3)
+        }, num_workers=min(3, cpu_count()))
 
         writer.from_indexed_dataset(dataset)
 
-        loader = Loader(name, batch_size=3, num_workers=5,
+        loader = Loader(name, batch_size=3, num_workers=min(5, cpu_count()),
                         pipelines={
                             'activation': [BytesDecoder()],
                             'index': [IntDecoder()]
