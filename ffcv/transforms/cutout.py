b04ec5b5d246edc1a8536f66a79e7fe5c47189db
Benjamin Warner <me@benjaminwarner.dev>
2023-03-06 15:37:29 -0600

    Add Random Cutout & Random Erasing

--- ffcv/transforms/cutout.py
+++ ffcv/transforms/cutout.py
@@ -2,6 +2,7 @@
 Cutout augmentation (https://arxiv.org/abs/1708.04552)
 """
 import numpy as np
+from numpy.random import rand
 from typing import Callable, Optional, Tuple
 from dataclasses import replace
 
@@ -50,3 +51,44 @@ class Cutout(Operation):
 
     def declare_state_and_memory(self, previous_state: State) -> Tuple[State, Optional[AllocationQuery]]:
         return replace(previous_state, jit_mode=True), None
+
+
+class RandomCutout(Cutout):
+    """Random cutout data augmentation (https://arxiv.org/abs/1708.04552).
+
+    Parameters
+    ----------
+    prob : float
+        Probability of applying on each image.
+    crop_size : int
+        Size of the random square to cut out.
+    fill : Tuple[int, int, int], optional
+        An RGB color ((0, 0, 0) by default) to fill the cutout square with.
+        Useful for when a normalization layer follows cutout, in which case
+        you can set the fill such that the square is zero post-normalization.
+    """
+    def __init__(self, prob: float, crop_size: int, fill: Tuple[int, int, int] = (0, 0, 0)):
+        super().__init__(crop_size, fill)
+        self.prob = np.clip(prob, 0., 1.)
+
+    def generate_code(self) -> Callable:
+        my_range = Compiler.get_iterator()
+        crop_size = self.crop_size
+        fill = self.fill
+        prob = self.prob
+
+        def cutout_square(images, *_):
+            should_cutout = rand(images.shape[0]) < prob
+            for i in my_range(images.shape[0]):
+                if should_cutout[i]:
+                    # Generate random origin
+                    coord = (
+                        np.random.randint(images.shape[1] - crop_size + 1),
+                        np.random.randint(images.shape[2] - crop_size + 1),
+                    )
+                    # Black out image in-place
+                    images[i, coord[0]:coord[0] + crop_size, coord[1]:coord[1] + crop_size] = fill
+            return images
+
+        cutout_square.is_parallel = True
+        return cutout_square
